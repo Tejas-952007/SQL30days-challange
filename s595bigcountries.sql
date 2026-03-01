cae281f9877df9949abaac2889975ebb8df6e2c7
Tejas-952007 <tejasingle137@gmail.com>
2026-03-01 13:26:54 +0530

    1757 recylable and low fats - 1/3/2026 solved

---
+++ s595bigcountries.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select
+   name,
+   population,
+   area
+from World
+where population >= 25000000
+union
+select name,population,area
+from world
+where area >= 3000000;
